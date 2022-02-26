cdf41ed6801b23a45d50111c713682ea545a1305
Sam Niyazi <24701208+SamNiyazi2@users.noreply.github.com>
2022-02-26 03:58:29 -0600

    Add project files.

---
+++ ps-343-webAPI/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// 02/26/2022 03:48 am - SSN - [20220226-0348] - [001] - M02-05 Demo - Creating an API project
+
+
+namespace ps_343_webAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
