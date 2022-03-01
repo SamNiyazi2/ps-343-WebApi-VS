7dc3edeef121542e7dbdbcd1812c7586d2c92478
Sam Niyazi <24701208+SamNiyazi2@users.noreply.github.com>
2022-03-01 14:03:33 -0600

    Add model validation.

--- ps-343-webAPI/Models/CourseCreateDTO.cs
+++ ps-343-webAPI/Models/CourseCreateDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,7 +10,15 @@ namespace ps_343_webAPI.Models
 {
     public class CourseCreateDTO
     {
+
+        // 03/01/2022 01:59 pm - SSN - [20220301-1246] - [001] - M07-03 - Demo: Validating input with data annotations
+
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
+
+        [MaxLength(1500)]
         public string Description { get; set; }
+
     }
 }
